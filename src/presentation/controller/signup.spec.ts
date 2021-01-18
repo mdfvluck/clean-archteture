7d9bd1a686d3b80e4bb8f93760dc0898f599d988
mdfvluck <luckmfv@gmail.com>
2021-01-18 13:45:30 -0300

    feat: ensure signUpCOntroller return an error if no email is provided

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -15,4 +15,19 @@ describe('SignUp Controller', () => {
     expect(httResponse.statusCode).toBe(400)
     expect(httResponse.body).toEqual(new Error('Missing params: name'))
   })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        // email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httResponse = sut.handle(httpRequest)
+    expect(httResponse.statusCode).toBe(400)
+    expect(httResponse.body).toEqual(new Error('Missing params: email'))
+  })
 })
--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,8 +1,14 @@
 export class SignUpController {
   handle (httpRquest: any): any {
+    if (!httpRquest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing params: name')
+      }
+    }
     return {
       statusCode: 400,
-      body: new Error('Missing params: name')
+      body: new Error('Missing params: email')
     }
   }
 }
