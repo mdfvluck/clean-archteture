924ef942ab33a43fde471bc7be5acb5a8a35ab59
mdfvluck <luckmfv@gmail.com>
2021-01-19 13:39:54 -0300

    refactor: import from index

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
 import { HttpResponse } from '../protocols'
-import { ServerError } from '../error/server-error'
+import { ServerError } from '../error'
 
 export const badRequest = (error: Error): HttpResponse => {
   return {
