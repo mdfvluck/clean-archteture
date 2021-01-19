dc4f7760adcf5a03e3ce602e1243c6650ab66b96
mdfvluck <luckmfv@gmail.com>
2021-01-19 09:57:17 -0300

    feat: ensure signupController return 200

--- src/presentation/controller/signup/signup.ts
+++ src/presentation/controller/signup/signup.ts
@@ -29,7 +29,7 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
@@ -37,7 +37,7 @@ export class SignUpController implements Controller {
 
       return {
         statusCode: 200,
-        body: ''
+        body: account
       }
     } catch {
       return serverError()
