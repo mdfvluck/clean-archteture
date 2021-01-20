61486b67b54ecd644f3f2c2d5cd05875016ef5cb
mdfvluck <luckmfv@gmail.com>
2021-01-20 16:08:57 -0300

    refactor: clear database between tests

--- src/data/infra/db/mongodb/account-repository/account.spec.ts
+++ src/data/infra/db/mongodb/account-repository/account.spec.ts
@@ -6,11 +6,21 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+
+  const makeSut = (): AccountMongoRepository => {
+    return new AccountMongoRepository()
+  }
+
   test('Should return an account on success', async () => {
-    const sut = new AccountMongoRepository()
+    const sut = makeSut()
     const account = await sut.add({
       name: 'any_name',
       email: 'any_email@email.com',
