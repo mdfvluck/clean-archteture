0768e712f78f42cb8784e4889bed70a4dbf570d0
mdfvluck <luckmfv@gmail.com>
2021-01-20 10:08:17 -0300

    feat: ensure dbAddAccount calls encrypter with correct password

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,6 +1,5 @@
 import { HttpRequest, HttpResponse } from './http'
 
 export interface Controller {
-  // eslint-disable-next-line @typescript-eslint/method-signature-style
   handle (httpRequest: HttpRequest): Promise<HttpResponse>
 }
