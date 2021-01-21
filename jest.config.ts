53e0bf21d9fefe08c0b08b01b564e206f9e4843a
mdfvluck <luckmfv@gmail.com>
2021-01-21 10:17:02 -0300

    feat: add expressRoute adapter

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,11 @@
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,20 @@
+import { MongoHelper } from '../../data/infra/db/mongodb/helper/mongo-helpers'
 import request from 'supertest'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
   test('Should return account on success', async () => {
     await request(app)
       .post('/api/signup')
