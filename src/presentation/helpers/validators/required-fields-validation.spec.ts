48d6cc9cd20f37a8b6864ddafc6dedc8cbb8861d
mdfvluck <luckmfv@gmail.com>
2021-01-26 11:29:02 -0300

    test: ensure required field validation not return if validation success

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -7,4 +7,10 @@ describe('Required Fields Validation', () => {
     const error = sut.validate({ another_field: 'any_value' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
+
+  test('Should return a missingParamError if validation fails', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate({ any_field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
