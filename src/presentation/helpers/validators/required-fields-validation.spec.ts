d9f073ce65ce147beb29d4be54a7680e1f1821a7
mdfvluck <luckmfv@gmail.com>
2021-01-26 11:32:17 -0300

    refactor: move sut to a factory pattern

--- src/presentation/helpers/validators/required-fields-validation.spec.ts
+++ src/presentation/helpers/validators/required-fields-validation.spec.ts
@@ -2,14 +2,23 @@ import { MissingParamError } from '../../error'
 import { RequiredFieldValidation } from './required-field-validation'
 
 describe('Required Fields Validation', () => {
-  test('Should return a missingParamError if validation fails', () => {
+  interface SutTypes {
+    sut: RequiredFieldValidation
+  }
+
+  const makeSut = (): SutTypes => {
     const sut = new RequiredFieldValidation('any_field')
+    return { sut }
+  }
+
+  test('Should return a missingParamError if validation fails', () => {
+    const { sut } = makeSut()
     const error = sut.validate({ another_field: 'any_value' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
 
   test('Should return a missingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('any_field')
+    const { sut } = makeSut()
     const error = sut.validate({ any_field: 'any_value' })
     expect(error).toBeFalsy()
   })
