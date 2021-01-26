dcb5ef31e185ad181943b3915b8b7155c1758e7d
mdfvluck <luckmfv@gmail.com>
2021-01-26 16:27:27 -0300

    refactor: fix import path from files

--- src/presentation/helpers/validators/email-validation.ts
+++ src/presentation/helpers/validators/email-validation.ts
@@ -1,6 +1,6 @@
 import { EmailValidator } from '../../protocols/email-validator'
 import { InvalidParamError } from '../../error'
-import { Validation } from './validation'
+import { Validation } from '../../protocols/validation'
 
 export class EmailValidation implements Validation {
   private readonly fieldName: string
